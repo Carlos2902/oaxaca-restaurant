350a90861274a718ae7f21bb12cb6fd6b0581fad
Carlos Lopez <carloslopezr29@gmail.com>
2024-01-30 10:23:06 -0500

    About us section added

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -1,10 +1,38 @@
 import React from 'react';
 
+import {images} from '../../constants';
 import './AboutUs.css';
 
 const AboutUs = () => (
-  <div>
-    AboutUs
+  <div className="app__aboutus app__bg flex__center section__padding" id="about">
+    {/* Letter G background */}
+    <div className="app__aboutus-overlay flex__center">
+      <img src={images.G} alt="g letter" />
+    </div>
+
+    {/* Left side about us content */}
+    <div className="app__aboutus-content flex__center">
+      <div className="app__aboutus-content_about">
+          <h1 className="headtext__cormorant">About us</h1>
+          <img src={images.spoon} alt="about_spoon" className="spoon__img"/>
+          <p className="p__opensans">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat veniam eveniet hic quia est molestiae, modi fuga eius nesciunt itaque vero impedit incidunt numquam magnam rem eligendi quidem alias soluta.</p>
+          <button type="button" className="custom__button">Know More</button>
+      </div>  
+      {/* Knife Img */}
+      <div className="app__aboutus-content_knife flex__center">
+        <img src={images.knife} alt="about_knife"/>
+      </div>
+      {/* Right side content */}
+      <div className="app__aboutus-content_history">
+          <h1 className="headtext__cormorant">Our history</h1>
+          <img src={images.spoon} alt="about_spoon" className="spoon__img"/>
+          <p className="p__opensans">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat veniam eveniet hic quia est molestiae, modi fuga eius nesciunt itaque vero impedit incidunt numquam magnam rem eligendi quidem alias soluta.</p>
+          <button type="button" className="custom__button">Know More</button>
+      </div> 
+    </div>
+
+      
+
   </div>
 );
 
