020d58c3ed48b952267a0b71b9c2874223ee4efa
Carlos Lopez <carloslopezr29@gmail.com>
2024-02-01 21:42:22 -0500

    Menu Items added

--- src/components/Menuitem/MenuItem.jsx
+++ src/components/Menuitem/MenuItem.jsx
@@ -2,9 +2,27 @@ import React from 'react';
 
 import './MenuItem.css';
 
-const MenuItem = () => (
-  <div>
-    MenuItem
+const MenuItem = ({title, price, tags}) => (
+  <div className="app__menuitem">
+    <div className="app__menuitem-head">
+      {/* Menu titles */}
+      <div className="app__menuitem-name">
+        <p className="p__cormorant" style={{color: '#DCCA87'}}>{title}</p>
+      </div>
+
+      {/* Dash between title & prices */}
+      <div className="app__menuitem-dash" />
+
+      {/* Menu prices */}
+      <div className="app__menuitem-price">
+        <p className="p__cormorant">{price}</p>
+      </div>
+    </div>
+
+    {/* SubTags|Bottle,etc. */}
+    <div className="app__menuitem-sub">
+      <p className="p__opensans" style={{color:'#AAA' }}>{tags}</p>
+    </div>
   </div>
 );
 
--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -9,7 +9,7 @@ const SpecialMenu = () => (
   <div className="app__specialMenu flex__center section__padding" id="menu">
     <div className="app__specialMenu-title">
       <SubHeading title="Menu that fits your palatte"/>
-        <h1 className="headtext_cormorant">Today's Special</h1>
+      <h1 className="headtext__cormorant">Today&apos;s Special</h1>
     </div>
     <div className="app__specialMenu-menu">
       {/* 3 sections */}
@@ -17,7 +17,7 @@ const SpecialMenu = () => (
           <p className="app__specialMenu_menu_heading">Wine & Mezcal</p>
           <div className="app__specialMenu_menu_items">
             {data.wines.map((wine, index)=>(
-              <MenuItem key={wine.title + index} title={wine.price} price={wine.price} tags={wine.tags}/>
+              <MenuItem key={wine.title + index} title={wine.title} price={wine.price} tags={wine.tags}/>
             ))}
           </div>
       </div>
@@ -29,10 +29,10 @@ const SpecialMenu = () => (
 
     {/* Left section */}
       <div className="app__specialMenu-menu_cocktails flex__center">
-          <p className="app__specialMenu_menu_heading">Wine & Mezcal</p>
+          <p className="app__specialMenu-menu_heading">Wine & Mezcal</p>
           <div className="app__specialMenu_menu_items">
             {data.cocktails.map((cocktail, index)=>(
-              <MenuItem key={cocktail.title + index} title={cocktail.price}  price={cocktail.price} tags={cocktail.tags}/>
+              <MenuItem key={cocktail.title + index} title={cocktail.title}  price={cocktail.price} tags={cocktail.tags}/>
               ))}
           </div>
       </div>
