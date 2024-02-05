3c14378070fc2768354e6325f5bb529b60bdd713
Carlos Lopez <carloslopezr29@gmail.com>
2024-02-05 13:28:25 -0500

    Awards & Laurels Responsive Design added

--- src/container/Laurels/Laurels.jsx
+++ src/container/Laurels/Laurels.jsx
@@ -7,6 +7,10 @@ import './Laurels.css';
 const AwardCard = ({award: {imgUrl, title, subtitle}})=>(
   <div className="app__laurels_awards-card">
     <img src={imgUrl} alt="award" />
+    <div className="app__laurels_awards-card_content">
+      <p className="p__cormorant" style={{color: "#DCCA87"}}>{title}</p>
+      <p className="p__cormorant">{subtitle}</p>
+    </div>
   </div>
 )
 
@@ -16,11 +20,13 @@ const Laurels = () => (
       <SubHeading title="Awards & Recognition"/>
       <h1 className="headtext__cormorant">Our Laurels</h1>
 
-      <div className="app__laurels_awards"></div>
+      <div className="app__laurels_awards">
       {data.awards.map((award) => <AwardCard award={award} key={award.title} />)}
+      </div>
     </div>
-    <div className="app__wrapper_img">
 
+    <div className="app__wrapper_img">
+        <img src={images.laurels} alt="laurels" />
     </div>
   </div>
 );
