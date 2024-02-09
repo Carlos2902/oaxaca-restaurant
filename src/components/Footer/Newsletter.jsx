7f3d7ef8f9991d3a9bc10c379930bf058b8c7f92
Carlos Lopez <carloslopezr29@gmail.com>
2024-02-09 13:00:21 -0500

    Newsletter styles added

--- src/components/Footer/Newsletter.jsx
+++ src/components/Footer/Newsletter.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import SubHeading from '../SubHeading/SubHeading';
+import {SubHeading} from '../../components';
 import './Newsletter.css';
 
 const Newsletter = () => {
@@ -8,12 +8,22 @@ const Newsletter = () => {
   console.log("Newsletter component is being rendered");
 
   return(
+    // Container first
   <div className="app__newsletter" >
     <div className="app__newsletter-heading">
       <SubHeading title="Newsletter"/>
+      <h1 className="headtext__cormorant">Subscribe To Our Newsletter</h1>
+      <p className="p__opensans">And Never Miss Lastes Updates!</p>
+    </div>
+    <div className="app__newsletter-input flex__center">
+      <input 
+      type="email"
+      placeholder="Enter your email address"/>
+      <button className="custom__button">Suscribe</button>
     </div>
   </div>
   );
   };
 
 export default Newsletter;
+ 
